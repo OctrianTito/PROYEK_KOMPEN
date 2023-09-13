281610ab9c2c840beb4b5a0d68b7f3f8c5512141
Octrian Adiluhung Tito Putra <octriantito@gmail.com>
2023-09-13 10:19:55 +0700

    Awal Project

---
+++ Kompen.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Kompen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String namaMahasiswa;
+        long nim;
+        byte semesterYangDilewati;
+        long nomorTelepon;
+        short jumlahAlpha;
+        int jumlahKompen;
+
+        System.out.print("Masukkan Nama Mahasiswa : ");  
+        namaMahasiswa = sc.nextLine();
+        System.out.print("Masukkan NIM : ");
+        nim = sc.nextLong();
+        System.out.print("Masukkan Semester Yang Dilewati : ");
+        semesterYangDilewati = sc.nextByte();
+        System.out.print("Masukkan Nomor Telepon : ");
+        nomorTelepon = sc.nextLong();
+        System.out.print("Jumlah alpha : ");
+        jumlahAlpha = sc.nextShort();
+                
+        System.out.println("Nama Mahasiswa = " + namaMahasiswa);
+        System.out.println("NIM = " + nim);
+        System.out.println("Semester Yang Dilewati = " + semesterYangDilewati);
+        System.out.println("Nomor Telepon = " +nomorTelepon);
+        System.out.println("Jumlah Alpha = " +jumlahAlpha);
+
+        jumlahKompen = jumlahAlpha * 2 * semesterYangDilewati;
+        System.out.println("Jumlah Kompen = " + jumlahKompen);
+    }
+}
