f80c3256f2355cbaaf87b24c91897c79defde76d
Octrian Adiluhung Tito Putra <octriantito@gmail.com>
2023-09-27 09:18:12 +0700

    Revisi kode dan tipe data

--- Beta/Fitur Login/Flowchartlogin.java
+++ Beta/Fitur Login/Flowchartlogin.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Flowchartlogin {
     public static void main(String[] args) {
-        String correctusername = "admin", correctpass = "adminpass", username, pass ;
+        int correctusername = 12345, correctpass = 12345, username, pass;
 
    
         Scanner sc = new Scanner(System.in);
     System.out.print(" Masukkan Username anda : ");
-        username= sc.nextLine();
+        username = sc.nextInt();
     System.out.print(" Masukkan pass : ");
-        pass = sc.nextLine();
+        pass = sc.nextInt();
 
-    if (username.equals(correctusername) && pass.equals(correctpass)){
+    if (username == correctusername && pass == correctpass){
         System.out.print("Anda berhasil login");
      } else { 
         System.out.print("Anda gagal login");
