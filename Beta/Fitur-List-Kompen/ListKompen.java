c5e0030ad66238cc4c20d261d59451d3995dac19
Faliirham <falimagemaulana64@gmail.com>
2023-10-25 09:38:03 +0700

    penambahan array

--- Beta/Fitur-List-Kompen/ListKompen.java
+++ Beta/Fitur-List-Kompen/ListKompen.java
@@ -3,25 +3,24 @@ import java.util.Scanner;
 public class ListKompen {
 
     public static void main(String[] args) {
-        String jenisKompen1, jenisKompen2 = "", konfirmasi;
+        String jeniskompen [] = new String [12]; 
+        String konfirmasi;
+        int i;
 
         Scanner sc = new Scanner(System.in);
-
+        //input jenis pekerjaan kompen 
+        for ( i = 0; i < jeniskompen.length; i++) {
         System.out.print("Masukkan jenis pekerjaan kompen yang ingin anda berikan : ");
-        jenisKompen1 = sc.nextLine();
-        System.out.print("Apakah anda ingin menambahkan pekerjaan lagi? (y/t): ");
+        jeniskompen[i] = sc.nextLine();
+        //proses pemilihan ingin menambahkan kompen
+        System.out.print("Apakah anda ingin menambahkan pekerjaan kompen lagi ? (y/n) : ");
         konfirmasi = sc.nextLine();
-
-
-        if (konfirmasi.equalsIgnoreCase("y")) {
-            System.out.print("Masukkan jenis pekerjaan kompen baru : ");
-            jenisKompen2 = sc.nextLine();
-
+            if (konfirmasi.equals("y")) {
+                System.out.print("Masukkan jenis pekerjaan kompen yang ingin anda berikan : ");
+                jeniskompen[i] = sc.nextLine();
+            }else{ 
+                break;}
         } 
-            
-            System.out.println("List kompen: ");
-            System.out.println(jenisKompen1);
-            System.out.println(jenisKompen2);
-        sc.close();    
+     sc.close(); 
     }
 }
