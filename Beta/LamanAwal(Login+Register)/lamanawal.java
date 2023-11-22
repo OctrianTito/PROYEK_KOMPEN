059cf1d350a270eca48d8fda139ec7e35132cdd6
Faliirham <falimagemaulana64@gmail.com>
2023-11-22 09:43:26 +0700

    Pemberian fungsi Login & Register

--- Beta/LamanAwal(Login+Register)/lamanawal.java
+++ Beta/LamanAwal(Login+Register)/lamanawal.java
@@ -1,65 +1,37 @@
 import java.util.Scanner;
 
 public class lamanawal {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        String[][] users = new String[100][2];
-        String[][] userAdmin = new String[3][2];
-        userAdmin[0][0] = "Admin1";
-        userAdmin[0][1] = "Admin45";
-        userAdmin[1][0] = "Admin2";
-        userAdmin[1][1] = "Admin245";
-        userAdmin[2][0] = "Admin3";
-        userAdmin[2][1] = "Admin345";
+    static Scanner sc = new Scanner(System.in);
+    //Fungsi Menu Login
+    static void Login(String[][]users, String[][] userAdmin,int numUsers){
+        String usernameInput, passInput;
+        System.out.print("Masukkan Username : ");
+        usernameInput = sc.nextLine();
+        System.out.print("Masukkan Password : ");
+        passInput = sc.nextLine();
 
-        char plh;
-        int numUsers = 0; // Untuk melacak jumlah pengguna yang sudah diregistrasi
-        boolean kembali = true;
+        boolean found = false;
 
-        do {
-            System.out.println("================================================");
-            System.out.println("------------------------------------------------");
-            System.out.println("||\t  SISTEM KOMPENSASI MAHASISWA \t      ||");
-            System.out.println("================================================");
-            System.out.println("\t\t1.Login   \t\t      ");
-            System.out.println("\t\t2.Register   \t\t      ");
-            System.out.print("\t\tpilih (1/2) :");
-            plh = sc.next().charAt(0);
-            sc.nextLine(); // Membersihkan newline dari buffer masukan
-            System.out.println("------------------------------------------------");
-            System.out.println("================================================");
-
-            switch (plh) {
-                case '1':
-                    String usernameInput, passInput;
-                    System.out.print("Masukkan Username : ");
-                    usernameInput = sc.nextLine();
-                    System.out.print("Masukkan Password : ");
-                    passInput = sc.nextLine();
-
-                    boolean found = false;
-
-                    for (int i = 0; i < numUsers || i < userAdmin.length; i++) {
-                            if (usernameInput.equals(users[i][0]) && passInput.equals(users[i][1])) {
-                                found = true;
-                                System.out.println("\tAnda berhasil login.");
-                                break;
-                            } else if (usernameInput.equals(userAdmin[i][0]) && passInput.equals(userAdmin[i][1])) {
-                                found = true;
-                                System.out.println("================================================");
-                                System.out.println("\tAnda berhasil Login sebagai Admin");
-                                
-                            }
-                        }
-                        
-                        if (!found) {
-                                System.out.println("Username atau password tidak terdaftar");
-                        }
-                        break;
-
-                case '2':
-                // Menu Rgister User 
+        for (int i = 0; i < numUsers || i < userAdmin.length; i++) {
+            if (usernameInput.equals(users[i][0]) && passInput.equals(users[i][1])) {
+                found = true;
+                System.out.println("\tAnda berhasil login.");
+                break;
+            } else if (usernameInput.equals(userAdmin[i][0]) && passInput.equals(userAdmin[i][1])) {
+                found = true;
+                System.out.println("================================================");
+                System.out.println("\tAnda berhasil Login sebagai Admin");    
+            }
+            
+            
+        }
+        if (!found) {
+                System.out.println("Username atau password tidak terdaftar");
+            }
+    }
+    // Fungsi Menu Rgister User
+    static void Register (String [][] users,int numUsers){
+         
                 System.out.println("================================================");
                 System.out.println("------------------------------------------------");
                 System.out.println("\t\tLaman Register User");
@@ -82,8 +54,42 @@ public class lamanawal {
                     
                     
                 } while (numUsers<users.length);
-                break;
-                    
+                
+    }
+    public static void main(String[] args) {
+        
+        String[][] users = new String[100][2];
+        String[][] userAdmin = new String[3][2];
+        userAdmin[0][0] = "Admin1";
+        userAdmin[0][1] = "Admin45";
+        userAdmin[1][0] = "Admin2";
+        userAdmin[1][1] = "Admin245";
+        userAdmin[2][0] = "Admin3";
+        userAdmin[2][1] = "Admin345";
+
+        char plh;
+        int numUsers = 0; // Untuk melacak jumlah pengguna yang sudah diregistrasi
+        boolean kembali = true;
+        do {
+            System.out.println("================================================");
+            System.out.println("------------------------------------------------");
+            System.out.println("||\t  SISTEM KOMPENSASI MAHASISWA \t      ||");
+            System.out.println("================================================");
+            System.out.println("\t\t1.Login   \t\t      ");
+            System.out.println("\t\t2.Register   \t\t      ");
+            System.out.print("\t\tpilih (1/2) :");
+            plh = sc.next().charAt(0);
+            sc.nextLine(); // Membersihkan newline dari buffer masukan
+            System.out.println("------------------------------------------------");
+            System.out.println("================================================");
+
+            switch (plh) {
+                case '1':
+                   Login(users, userAdmin, numUsers);
+                   break;
+                case '2':
+                    Register(users, numUsers);
+                    break;
                 
             default:
 
@@ -103,5 +109,5 @@ public class lamanawal {
                 kembali = false; // Keluar dari program jika ingin kembali
             }
         } while (kembali);
-    }
+    } 
 }
